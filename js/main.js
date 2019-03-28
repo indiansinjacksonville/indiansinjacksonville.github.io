80a87424dddf0f07d9c0b9dc55f667f9e9cbb341
indiansinjacksonville <48895302+indiansinjacksonville@users.noreply.github.com>
2019-03-27 22:16:17 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 
-$(document).ready(function() {
-  $(window).keydown(function(event){
-    if(event.keyCode == 13) {
-      event.preventDefault();
-    }
-  });
-});
 
 
 ///Search engine code from codepen///
